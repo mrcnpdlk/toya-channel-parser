1373b15de6acb3367b454a5e6e66a0502d792f1a
Marcin Pudełek <mpudelek@widzew.net>
2016-10-30 00:18:47 +0200

    gatting better quality channel list

--- src/Site.php
+++ src/Site.php
@@ -48,8 +48,10 @@ class Site
         $offers = $dom->find('#main_body', 0)->find('.offers')->find('.offer');
 
         foreach ($offers as $offer) {
-            $chennalesArray = array();
-            $channels       = $offer->find('.offer-more-content-hidden')->find('.offer-more-channel-wrapper')->find('.offer-more-channel');
+            $packageName        = $offer->find('.offer-title')->innerHtml;
+            $channalesArray     = array();
+            $channalesRealArray = array();
+            $channels           = $offer->find('.offer-more-content-hidden')->find('.offer-more-channel-wrapper')->find('.offer-more-channel');
 
             foreach ($channels as $channel) {
                 $channelName = trim($channel->find('.channel-tooltip')->find('img')->getAttribute('alt'));
@@ -60,15 +62,35 @@ class Site
                 } else {
                     throw new Exception\Error("Not found channel ID for {$channelName}", 1);
                 }
-                $chennalesArray[] = array(
+
+                $isHd = preg_match('/HD$/', $channelName) ? true : false;
+
+                $channalesArray[] = array(
                     'id'   => $channelId,
                     'name' => $channelName,
+                    'isHd' => $isHd,
                 );
             }
+
+            usort($channalesArray, function ($item1, $item2) {
+                return strtolower($item1['name']) <=> strtolower($item2['name']);
+            });
+
+            foreach ($channalesArray as $key => $pos) {
+                if (isset($channalesArray[$key + 1]) && preg_match("/{$pos['name']} HD/", $channalesArray[$key + 1]['name'])) {
+                    $channalesArray[$key]['betterQuality'] = $channalesArray[$key + 1];
+                } else {
+                    $channalesArray[$key]['betterQuality'] = null;
+                    $channalesRealArray[]                  = $channalesArray[$key];
+                }
+            }
+
             $answer[] = array(
-                'packageName'   => $offer->find('.offer-title')->innerHtml,
-                'channelsCount' => count($chennalesArray),
-                'channels'      => $chennalesArray,
+                'packageName'       => $packageName,
+                'channelsAllCount'  => count($channalesArray),
+                'channelsAll'       => $channalesArray,
+                'channelsRealCount' => count($channalesRealArray),
+                'channelsReal'      => $channalesRealArray,
             );
         }
         return $answer;
@@ -79,7 +101,7 @@ class Site
         $CachedString = $this->oApp->fileCache->getItem($key);
 
         if (is_null($CachedString->get())) {
-            $CachedString->set($this->parseChannels())->expiresAfter(60);
+            $CachedString->set($this->parseChannels())->expiresAfter(1);
             $this->oApp->fileCache->save($CachedString);
             $htmlContent = $CachedString->get();
 
